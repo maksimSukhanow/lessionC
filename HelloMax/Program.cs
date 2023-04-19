91e74d23a99cdb3ed12b50ba418282fbbf1f8a81
MaksimSukhanov <filmskram@gmail.com>
2023-04-19 13:07:57 +0300

    Hello max

---
+++ HelloMax/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine(); 
+
+if(username.ToLower() == "max")
+{
+    Console.WriteLine("Well hello, is it Great Max!");
+}
+else
+{
+    Console.WriteLine("hi, ");
+    Console.WriteLine(username);
+}
