f178697de933afa68b1882d6834e42947c48907c
MaksimSukhanov <filmskram@gmail.com>
2023-04-19 12:49:24 +0300

    add random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 231;
-int numberB = 234;
+﻿int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,100);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
--- Example004/Program.cs
+++ Example004/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿double numberA = 25;
+double numberB = 12;
+Console.WriteLine(numberA / numberB);
